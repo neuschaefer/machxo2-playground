65f603b6695d4eb810b3cc8bdcd8a309244963fa
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2023-02-25 10:43:26 +0100

    Improve build script

---
+++ build.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import argparse, sys
+
+from settings import ssh_settings
+
+from amaranth_boards.machxo2_breakout import *
+from amaranth_boards.test.blinky import Blinky
+from amaranth.back import verilog
+from led_runner import *
+
+def get_design(name):
+    match args.design.lower():
+        case 'ledrunner': return LEDRunner()
+        case 'boringledrunner': return BoringLEDRunner()
+        case 'blinky': return Blinky()
+        case _:
+            print(f'Unknown design "{name}"')
+            sys.exit(1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MachXO2 build script')
+    parser.add_argument('--osch-frequency', default='2.08', help='Internal oscillator frequency')
+    parser.add_argument('design', default='ledrunner', nargs='?', help='design name')
+    parser.add_argument('--verilog', type=argparse.FileType('w'), help='verilog file to export')
+    args = parser.parse_args()
+
+    p = MachXO2_7000HE_BreakoutPlatform()
+    p.osch_frequency = float(args.osch_frequency)
+    design = get_design(args.design)
+
+    if args.verilog:
+        output = verilog.convert(design, 'top', p, ports=())
+        args.verilog.write(output)
+    else:
+        plan = p.prepare(design)
+        prod = plan.execute_remote_ssh(connect_to=ssh_settings, root='/tmp/fpga-build')
+        p.toolchain_program(prod, 'top')
