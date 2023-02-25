fe12a324df6d09250d69bb61c47776a91ee26284
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2023-02-25 10:42:19 +0100

    Make LEDRunner more interesting

--- led_runner.py
+++ led_runner.py
@@ -2,26 +2,27 @@ from amaranth import *
 from amaranth.build.res import *
 import itertools
 
+def get_all_resources(platform, name):
+    resources = []
+    for number in itertools.count():
+        try:
+            resources.append(platform.request(name, number))
+        except ResourceError:
+            break
+    return resources
+
 class LEDRunner(Elaboratable):
+    cool = True
 
     def elaborate(self, platform):
-
-        def get_all_resources(name):
-            resources = []
-            for number in itertools.count():
-                try:
-                    resources.append(platform.request(name, number))
-                except ResourceError:
-                    break
-            return resources
-
         m = Module()
 
-        half_freq = int(platform.default_clk_frequency)
-        timer = Signal(range(half_freq + 1))
+        freq = int(platform.default_clk_frequency)
+        timer = Signal(range(freq))
+        print(f'timer length {len(timer)}')
         m.d.sync += timer.eq(timer + 1)
 
-        leds = get_all_resources('led')
+        leds = get_all_resources(platform, 'led')
 
         position0 = Signal(range(len(leds)))
         position = Signal(range(len(leds)))
@@ -29,9 +30,13 @@ class LEDRunner(Elaboratable):
 
         m.d.comb += Cat(position0, invert).eq(timer[-len(position0)-1:])
         m.d.comb += position.eq(position0)
-        with m.If(invert):
-            m.d.comb += position.eq(~position0)
+        if self.cool:
+            with m.If(invert):
+                m.d.comb += position.eq(~position0)
 
         m.d.comb += Cat(leds).eq(1 << position)
 
         return m
+
+class BoringLEDRunner(LEDRunner):
+    cool = False
