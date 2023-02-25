8edff2fdd5d52a8df83496bfdd09a7fffb01753c
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2023-02-25 10:58:45 +0100

    Allow local builds and different toolchains

--- build.py
+++ build.py
@@ -23,9 +23,11 @@ if __name__ == '__main__':
     parser.add_argument('--osch-frequency', default='2.08', help='Internal oscillator frequency')
     parser.add_argument('design', default='ledrunner', nargs='?', help='design name')
     parser.add_argument('--verilog', type=argparse.FileType('w'), help='verilog file to export')
+    parser.add_argument('--local', help='run build locally', action='store_true')
+    parser.add_argument('--toolchain', help='toolchain to use (Diamond or Trellis)', default='Diamond')
     args = parser.parse_args()
 
-    p = MachXO2_7000HE_BreakoutPlatform()
+    p = MachXO2_7000HE_BreakoutPlatform(toolchain=args.toolchain)
     p.osch_frequency = float(args.osch_frequency)
     design = get_design(args.design)
 
@@ -34,5 +36,8 @@ if __name__ == '__main__':
         args.verilog.write(output)
     else:
         plan = p.prepare(design)
-        prod = plan.execute_remote_ssh(connect_to=ssh_settings, root='/tmp/fpga-build')
+        if args.local:
+            prod = plan.execute()
+        else:
+            prod = plan.execute_remote_ssh(connect_to=ssh_settings, root='/tmp/fpga-build')
         p.toolchain_program(prod, 'top')
